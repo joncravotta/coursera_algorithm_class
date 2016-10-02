9d6af45dbf9275121fe5b009ba5f554b69bb76e2
Jon Cravotta <joncravotta32@gmail.com>
2016-10-02 15:46:19 -0400

    majority element

--- section_one/week_four/majority_element/majority_element.py
+++ section_one/week_four/majority_element/majority_element.py
@@ -5,8 +5,24 @@ def get_majority_element(a, left, right):
     if left == right:
         return -1
     if left + 1 == right:
-        return a[left]
-    #write your code here
+        return int(a[left])
+
+    left_m = get_majority_element(a, left, (left + right - 1)//2 + 1)
+    right_m = get_majority_element(a, (left + right - 1)//2 + 1, right)
+    left_count = 0
+    for i in range(left, right):
+        if a[i] == left_m:
+            left_count += 1
+    if left_count > (right-left)//2:
+        return int(left_m)
+
+    right_count = 0
+    for i in range(left, right):
+        if a[i] == right_m:
+            right_count += 1
+    if right_count > (right-left)//2:
+        return int(right_m)
+
     return -1
 
 if __name__ == '__main__':
